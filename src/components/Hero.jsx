82a78f8aaa20a66c6043fcc0e135a3d455276381
enosintech <enos.nsambaj@gmail.com>
2024-07-19 16:13:14 +0200

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,20 +37,20 @@ const Hero = () => {
   return (
     <section className="w-full h-screen pt-[130px] relative hero">
         <div className="w-full h-full px-2 pt-2">
-            <div className="w-full h-full overflow-hidden flex common-padding pt-5 pb-10">
-                <div className="w-1/2 h-full flex flex-col justify-end gap-y-10 text-black">
-                  <p className="text-[1.7vw] font-extrabold text-justify max-w-[90%]">Rusangu University is a co-educational institution of higher learning jointly owned and operated by the Rusangu Conference of Seventh-day Adventists</p>
+            <div className="w-full h-full overflow-hidden flex">
+                <div className="w-1/2 h-full flex flex-col 2xl:pl-52 xl:pl-48 lg:pl-40 md:pl-36 pl-28 justify-end pb-14 gap-y-10 text-black">
+                  <p className="2xl:text-[2vw] xl:text-[1.8vw] lg:text-[1.8vw] font-[500] text-justify max-w-[93%]">Rusangu University is a co-educational institution of higher learning jointly owned and operated by the Rusangu Conference of Seventh-day Adventists</p>
                   <button className="bg-primary w-fit h-fit px-10 py-3 hover-action">
                     <p className="tracking-tight font-medium text-[1.2vw] text-white">APPLY NOW</p>
                   </button>
                   <p className="text-[1.2vw] font-medium tracking-tight">SCROLL TO DISCOVER <span className="text-primary">YOUR NEXT CHALLENGE</span></p>
                 </div>
-                <div className="w-1/2 h-full">
-                  <div className="w-full h-full relative deep-shadow">
-                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[4] deep-shadow" src={rusanguCampusImg} alt="hero Image 1"/>
-                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[3] deep-shadow" src={slideshow3Img} alt="hero Image 2"/>
-                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[2] deep-shadow" src={slideshow4Img} alt="hero Image 3"/>
-                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[1] deep-shadow" src={slideshow5Img} alt="hero Image 4"/>
+                <div className="w-1/2 h-full pb-2">
+                  <div className="w-full h-full relative deep-shadow overflow-hidden">
+                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[4] " src={rusanguCampusImg} alt="hero Image 1"/>
+                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[3] " src={slideshow3Img} alt="hero Image 2"/>
+                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[2] " src={slideshow4Img} alt="hero Image 3"/>
+                    <img className="w-full h-full object-cover motiv absolute top-0 left-0 z-[1] " src={slideshow5Img} alt="hero Image 4"/>
                   </div>
                 </div>
                 <div className="w-fit h-fit flex flex-col items-center gap-y-5 absolute bottom-0 left-0 right-0 mx-auto z-[5] text-center text-white">
--- src/components/NavOptions.jsx
+++ src/components/NavOptions.jsx
@@ -10,7 +10,7 @@ const NavOptions = () => {
         {navOptions.map((item, idx) => (
             <div key={item.id} className="w-1/4 h-full relative group">
                 <img className="absolute z-[1] w-full h-full object-cover" src={item.image} alt="choices" />
-                <div className="absolute z-[2] w-full h-full bg-[rgba(255,188,205,0.6)] group-hover:opacity-0 transition-all duration-200"></div>
+                <div className="absolute z-[2] w-full h-full bg-[rgba(254,210,221,0.6)] group-hover:opacity-0 transition-all duration-200"></div>
                 <div className="absolute z-[3] left-0 right-0 mx-auto max-w-[80%] h-full flex flex-col items-center justify-center gap-y-6 text-white">
                     <FontAwesomeIcon icon={item.icon} className="text-[1.8vw]"/>
                     <p className="font-medium text-[1.2vw]">{item.name}</p>
